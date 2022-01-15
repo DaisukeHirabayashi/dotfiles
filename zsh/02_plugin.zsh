dc0c1c3baeb3a5c13b20b4285a1ab2b91ccb5a88
平林大典 <daisuke@pluslab.org>
2022-01-15 17:49:33 +0900

    add syntax hilight plugin

--- zsh/02_plugin.zsh
+++ zsh/02_plugin.zsh
@@ -1,6 +1,9 @@
 # powerlevel10k
 zinit ice depth=1; zinit light romkatv/powerlevel10k
 
+# syntax-highlighting
+zinit light zdharma-continuum/fast-syntax-highlighting
+
 # autosuggestions
 zinit light zsh-users/zsh-autosuggestions
 bindkey '^j' autosuggest-accept
