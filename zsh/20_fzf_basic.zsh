2a6b9ee05e3fec59e2042d33fa1a996826989254
DaisukeHirabayashi <daisuke@pluslab.org>
2022-01-17 13:37:46 +0900

    move directory and alias for nvim

--- zsh/20_fzf_basic.zsh
+++ zsh/20_fzf_basic.zsh
@@ -5,3 +5,12 @@ fbr() {
            fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
   git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
+
+fcd() {
+  local dir
+  dir=$(find ${1:-.} -path '*/\.*' -prune \
+                  -o -type d -print 2> /dev/null | fzf +m) &&
+  cd "$dir"
+}
+
+alias fvi='vim $(fzf)'
