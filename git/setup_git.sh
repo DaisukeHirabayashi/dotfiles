878dd3c921c8914a82bfc50ca4d396033ad99ea5
DaisukeHirabayashi <daisuke@pluslab.org>
2022-03-27 16:02:21 +0900

    echoの位置の調整

--- git/setup_git.sh
+++ git/setup_git.sh
@@ -3,4 +3,5 @@ read -p "Github Mail Address: " mail_address
 echo "[user]" >> ${HOME}/.gitconfig.user
 echo "  name = ${user_name}" >> ${HOME}/.gitconfig.user
 echo "  email = ${mail_address}" >> ${HOME}/.gitconfig.user
+echo "create the file ${HOME}/.gitconfig.user for user information"
 
