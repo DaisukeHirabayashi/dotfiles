36260da7ee3870761e04105f25d51fc7f73f78e4
DaisukeHirabayashi <daisuke@pluslab.org>
2022-03-27 16:31:31 +0900

    setupするかどうかを選択できるように変更

--- git/setup_git.sh
+++ git/setup_git.sh
@@ -1,7 +1,12 @@
-read -p "Github User Name: " user_name
-read -p "Github Mail Address: " mail_address
-echo "[user]" >> ${HOME}/.gitconfig.user
-echo "  name = ${user_name}" >> ${HOME}/.gitconfig.user
-echo "  email = ${mail_address}" >> ${HOME}/.gitconfig.user
-echo "create the file ${HOME}/.gitconfig.user for user information"
+read -p "Do you want to change your user information? [y,n]" ans
+case "$ans" in
+  [yY]*)  rm ${HOME}/.gitconfig.user
+    read -p "Github User Name: " user_name
+    read -p "Github Mail Address: " mail_address
+    echo "[user]" >> ${HOME}/.gitconfig.user
+    echo "  name = ${user_name}" >> ${HOME}/.gitconfig.user
+    echo "  email = ${mail_address}" >> ${HOME}/.gitconfig.user
+    echo "create the file ${HOME}/.gitconfig.user for user information" ;;
+  *) ;;
+esac
 
