3b73437a3f364f761c6696c3978e78f7b1ecd707
DaisukeHirabayashi <daisuke@pluslab.org>
2022-04-30 12:05:30 +0900

    gitのlocalの内容からファイルを作り直すように変更

--- git/setup_git.sh
+++ git/setup_git.sh
@@ -1,11 +1,10 @@
 read -p "Do you want to change your user information? [y,n]" ans
 case "$ans" in
-  [yY]*)  rm ${HOME}/.gitconfig.user
-    read -p "Github User Name: " user_name
-    read -p "Github Mail Address: " mail_address
-    echo "[user]" >> ${HOME}/.gitconfig.user
-    echo "  name = ${user_name}" >> ${HOME}/.gitconfig.user
-    echo "  email = ${mail_address}" >> ${HOME}/.gitconfig.user
+  [yY]*)  echo "[user]" >> ${HOME}/.gitconfig.user_tmp
+    echo "  name = `git config user.name`" >> ${HOME}/.gitconfig.user_tmp
+    echo "  email = `git config user.email`" >> ${HOME}/.gitconfig.user_tmp
+    rm ${HOME}/.gitconfig.user
+    mv ${HOME}/.gitconfig.user_tmp ${HOME}/.gitconfig.user
     echo "create the file ${HOME}/.gitconfig.user for user information" ;;
   *) ;;
 esac
