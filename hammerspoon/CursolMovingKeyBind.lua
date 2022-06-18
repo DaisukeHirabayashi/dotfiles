7e8df55b83f94004b021a3d794e54784e5dc42de
DaisukeHirabayashi <daisuke@pluslab.org>
2022-06-18 21:25:01 +0900

    itermでも使えるように変更

--- hammerspoon/CursolMovingKeyBind.lua
+++ hammerspoon/CursolMovingKeyBind.lua
@@ -10,32 +10,6 @@ local function remap(mods, key, pressFn)
   hs.hotkey.bind(mods, key, pressFn, nil, pressFn)  
 end
 
-local function disableAllHotkeys()
-   for k, v in pairs(hs.hotkey.getHotkeys()) do
-      v['_hk']:disable()
-   end
-end
-
-local function enableAllHotkeys()
-   for k, v in pairs(hs.hotkey.getHotkeys()) do
-      v['_hk']:enable()
-   end
-end
-
-local function handleGlobalAppEvent(name, event, app)
-   if event == hs.application.watcher.activated then
-      if name ~= "iTerm2" then
-         enableAllHotkeys()
-      else
-         disableAllHotkeys()
-      end
-   end
-end
-
-appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
-appsWatcher:start()
-
-
 remap({'ctrl'}, 'k', pressFn('up'))
 remap({'ctrl'}, 'h', pressFn('left'))
 remap({'ctrl'}, 'j', pressFn('down'))
