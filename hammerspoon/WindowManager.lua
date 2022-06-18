7bf6ad103ea70f1e028693ad27bfbc4b519f5342
DaisukeHirabayashi <daisuke@pluslab.org>
2022-06-18 20:35:52 +0900

    ctrlキーでカーソル移動ができるように変更

--- hammerspoon/WindowManager.lua
+++ hammerspoon/WindowManager.lua
@@ -1,38 +1,42 @@
---- start move cursor bewteen screens
-function show_circle()
-  local mousepoint = hs.mouse.getAbsolutePosition()
-  local color = {["red"]=0,["blue"]=1,["green"]=0,["alpha"]=0.5}
-  local circle = hs.drawing.circle(hs.geometry.rect(mousepoint.x - 40, mousepoint.y - 40, 80, 80))
-  circle:setStrokeColor(color)
-  circle:setFill(false)
-  circle:setStrokeWidth(5)
-  circle:bringToFront(true)
-  circle:show(0.5)
-  hs.timer.doAfter(0.8, function()
-    circle:delete()
-  end)
-end
-
-function move_cursor(direction)
-  return function()
-    local screen = hs.mouse.getCurrentScreen()
-    local nextScreen
-    if direction == "right" then
-      nextScreen = screen:next()
-    else
-      nextScreen = screen:previous()
-    end
-
-    local rect = nextScreen:fullFrame()
-    -- get the center of the rect
-    local center = hs.geometry.rect(rect).center
-    hs.mouse.setAbsolutePosition(center)
-    show_circle()
+local function pressFn(mods, key)
+  if key == nil then
+    key = mods
+    mods = {}
   end
+  return function() hs.eventtap.keyStroke(mods, key, 1000) end
 end
 
--- カーソルを右スクリーンに移動 --
-hs.hotkey.bind({"cmd"}, "r", move_cursor('right'))
+local function remap(mods, key, pressFn)
+  hs.hotkey.bind(mods, key, pressFn, nil, pressFn)  
+end
 
--- カーソルを左スクリーンに移動 --
-hs.hotkey.bind({"cmd"}, "l", move_cursor('left'))
+local function disableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:disable()
+   end
+end
+
+local function enableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:enable()
+   end
+end
+
+local function handleGlobalAppEvent(name, event, app)
+   if event == hs.application.watcher.activated then
+      if name ~= "iTerm2" then
+         enableAllHotkeys()
+      else
+         disableAllHotkeys()
+      end
+   end
+end
+
+appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
+appsWatcher:start()
+
+
+remap({'ctrl'}, 'k', pressFn('up'))
+remap({'ctrl'}, 'h', pressFn('left'))
+remap({'ctrl'}, 'j', pressFn('down'))
+remap({'ctrl'}, 'l', pressFn('right'))
