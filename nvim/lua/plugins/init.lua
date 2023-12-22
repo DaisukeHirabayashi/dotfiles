16b155393d02f56b9f25d22c2f39933e72953383
DaisukeHirabayashi <daisuke@pluslab.org>
2023-12-22 10:33:29 +0900

    lint

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -80,11 +80,11 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
-      local options = {	
-        theme = 'codedark'	
-      }	
-      require('lualine').setup {	
-        options = options,	
+      local options = {
+        theme = 'codedark'
+      }
+      require('lualine').setup {
+        options = options,
       }
     end
   },
