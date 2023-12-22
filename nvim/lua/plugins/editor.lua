1a3df791342dafbf5e152b50982e2263b301f871
DaisukeHirabayashi <daisuke@pluslab.org>
2023-12-22 13:57:11 +0900

    一旦追加

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,11 +1,52 @@
 return {
   {
+    'nvim-telescope/telescope.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope-ui-select.nvim'
+    },
+    config = function()
+      vim.keymap.set('n', ',f', require('telescope.builtin').find_files)
+      vim.keymap.set('n', ',g', require('telescope.builtin').git_files)
+      vim.keymap.set('n', ',b', require('telescope.builtin').buffers)
+      vim.keymap.set('n', ',r', require('telescope.builtin').live_grep)
+      vim.keymap.set('n', '<leader>d', function() require('telescope.builtin').diagnostics { bufnr = 0 } end)
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
+
+      local actions = require "telescope.actions"
+      require('telescope').setup {
+        defaults = {
+          sorting_strategy = "ascending",
+          selection_caret = "> ",
+          layout_config = { prompt_position = "top" },
+          mappings = {
+            i = {
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+              ["<Tab>"] = actions.move_selection_next,
+              ["<S-Tab>"] = actions.move_selection_previous,
+              ["<esc>"] = actions.close
+            }
+          }
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true,                    -- false will only do exact matching
+            override_generic_sorter = false, -- override the generic sorter
+            override_file_sorter = true,     -- override the file sorter
+            case_mode = "smart_case"         -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+          }
+        }
+      }
+
+      require("telescope").load_extension("ui-select")
+    end
+  },
+  {
     'neoclide/coc.nvim',
     config = function()
-      vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', { silent = true })
-      vim.api.nvim_set_keymap('n', 'gy', '<Plug>(coc-type-definition)', { silent = true })
-      vim.api.nvim_set_keymap('n', 'gi', '<Plug>(coc-implementation)', { silent = true })
-      vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', { silent = true })
+      require('plugin_configs/coc_settings')
     end
   },
   {
