9939b0516354762b24bffd2921ade1dc94c2cdd4
DaisukeHirabayashi <daisuke@pluslab.org>
2023-12-25 23:23:30 +0900

    fzfに夜検索を可能にする

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -3,7 +3,8 @@ return {
     'nvim-telescope/telescope.nvim',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      'nvim-telescope/telescope-ui-select.nvim'
+      'nvim-telescope/telescope-ui-select.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
     config = function()
       vim.keymap.set('n', ',f', require('telescope.builtin').find_files)
@@ -40,6 +41,7 @@ return {
         }
       }
 
+      require('telescope').load_extension('fzf')
       require("telescope").load_extension("ui-select")
     end
   },
